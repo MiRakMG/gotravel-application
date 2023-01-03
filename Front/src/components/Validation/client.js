0d0cdf1a28a449940245a3c0c0c06c0ed67fbbc9
ROBUSTE Manohisafidy <robustemmanuel@gmail.com>
2023-01-03 21:53:28 +0300

    fix bug validation

--- Front/src/components/Validation/client.js
+++ Front/src/components/Validation/client.js
@@ -5,5 +5,5 @@ export const schemaClient = Yup.object().shape({
   number: Yup.number()
     .required("Nombre de client obligatoire") // Sets it as a compulsory field
     .positive("Valeur positif s'il vous plaît!") // Validates against negative values
-    .min(1, "Le nombre de personne doit être supérieur à 0"), // Sets a minimum value});
+    .min(1, "Le nombre de personne doit être supérieur à 0"), // Sets a minimum value
 });
