4c02d05ee817cbd402253b3cddee1f5fb1528e8d
ROBUSTE Manohisafidy <robustemmanuel@gmail.com>
2023-01-06 09:51:00 +0300

    verification date for each endpoint

--- API-m/src/Controllers/TypeController.ts
+++ API-m/src/Controllers/TypeController.ts
@@ -3,23 +3,31 @@ import { AppDataSource } from "../data-source";
 import { Type } from "../entity/Type";
 
 export class TypeController {
-
-    public static async create(req: Request, res: Response) 
-    {
-        const type = await AppDataSource.manager.save(Type, {
-            ...req.body
-        });
-
-        return res.status(201).json(type);
+  public static async create(req: Request, res: Response) {
+    let type = await AppDataSource.manager.find(Type, {
+      where: {
+        wording_type: req.body.wording_type,
+      },
+    });
+    if (type?.length === 0) {
+      type = await AppDataSource.manager.save(Type, {
+        ...req.body,
+      });
     }
 
-    public static async update(req: Request, res: Response) 
-    {
-        await AppDataSource.manager.update(Type, {code_type: req.params.id}, {...req.body});
+    return res.status(201).json(type);
+  }
 
-        return res.status(201).json({
-            status: "Success",
-            message: "Updated successfully"
-        });
-    }
+  public static async update(req: Request, res: Response) {
+    await AppDataSource.manager.update(
+      Type,
+      { code_type: req.params.id },
+      { ...req.body }
+    );
+
+    return res.status(201).json({
+      status: "Success",
+      message: "Updated successfully",
+    });
+  }
 }
