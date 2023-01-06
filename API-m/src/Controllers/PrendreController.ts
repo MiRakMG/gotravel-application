b51a8d7df521cd5f440371b0d65b37c2d2096525
ROBUSTE Manohisafidy <robustemmanuel@gmail.com>
2023-01-06 03:59:45 +0300

    update prendre entity (verify if date is exist)

--- API-m/src/Controllers/PrendreController.ts
+++ API-m/src/Controllers/PrendreController.ts
@@ -1,15 +1,24 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../data-source";
 import { Prendre } from "../entity/Prendre";
-
+import { Date } from "../entity/Date";
 export class PrendreController {
-
-    public static async create(req: Request, res: Response) 
-    {
-        const take = await AppDataSource.manager.save(Prendre, {
-            ...req.body
-        });
-
-        return res.status(201).json(take);
+  public static async create(req: Request, res: Response) {
+    const findDate = await AppDataSource.manager.find(Date, {
+      where: {
+        date: req.body.date,
+      },
+    });
+    if (findDate?.length === 0) {
+      const date = await AppDataSource.manager.save(Date, {
+        date: req.body.date,
+      });
     }
+
+    const take = await AppDataSource.manager.save(Prendre, {
+      ...req.body,
+    });
+
+    return res.status(201).json(take);
+  }
 }
